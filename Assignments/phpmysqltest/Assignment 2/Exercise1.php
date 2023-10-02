54f5400d998660a7dda37187eddf5fdde38e9cce
JBehe <90488953+JBehe@users.noreply.github.com>
2023-10-02 00:33:29 -0400

    Add files via upload

---
+++ Assignments/phpmysqltest/Assignment 2/Exercise1.php
@@ -0,0 +1,65 @@
+<html lang="en">
+<head>   
+<meta charset="UTF-8">
+<?php
+// I had started to make a for loop inside the php portion realised php isn't nesscarry but could still be used
+
+
+/*$listNumber = 0;
+$listContentNumber = 0;
+
+for ($listNumber; $listNumber < )*/
+
+
+?>
+
+
+
+
+</head>
+
+
+
+
+<body>
+    
+    <ul>
+        <li> 1 </li>
+        <ul>
+            <li> 1 </li>
+            <li> 2 </li>
+            <li> 3 </li>
+            <li> 4 </li>
+            <li> 5 </li>
+        </ul>
+    </ul>
+    <ul>
+        <li> 2 </li>
+        <ul>
+            <li> 1 </li>
+            <li> 2 </li>
+            <li> 3 </li>
+            <li> 4 </li>
+            <li> 5 </li>
+        </ul>
+    </ul>
+    <li> 3 </li>
+        <ul>
+            <li> 1 </li>
+            <li> 2 </li>
+            <li> 3 </li>
+            <li> 4 </li>
+            <li> 5 </li>
+        </ul>
+    </ul>
+    <li> 4 </li>
+        <ul>
+            <li> 1 </li>
+            <li> 2 </li>
+            <li> 3 </li>
+            <li> 4 </li>
+            <li> 5 </li>
+        </ul>
+    </ul>
+    
+</html>
